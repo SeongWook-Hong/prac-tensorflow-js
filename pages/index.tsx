aa80bd40b020ebb1a6aebd6ca66c6455fb256cc0
wook <hsw5971@naver.com>
2025-05-08 23:27:42 +0900

    남의모델이용하기

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,16 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import Script from "next/script";
 
 export default function Home() {
   const imgRef = useRef<HTMLImageElement>(null);
-
+  const [result, setResult] = useState(false);
   useEffect(() => {
     const loadModel = async () => {
       if (!imgRef.current || !(window as any).mobilenet) return;
       const model = await (window as any).mobilenet.load();
       const predictions = await model.classify(imgRef.current);
       console.log("Predictions:", predictions);
+      setResult(true);
     };
 
     loadModel();
@@ -30,6 +31,7 @@ export default function Home() {
       {/* 이미지와 실행 코드 */}
       <h1>Image Classification</h1>
       <img ref={imgRef} src="/cat.jpg" alt="cat" width={300} />
+      <div>{result && "콘솔 창에서 결과를 확인하세요."}</div>
     </>
   );
 }
